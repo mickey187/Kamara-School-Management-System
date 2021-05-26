ff135ed45f6f218d40b64443032c2a668383d077
Mickey <mickeyhailu2@gmail.com>
2021-05-26 14:46:09 +0300

    second commit

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.master')
 @section('content')
 
-<button type="submit">Click</button>
+<section class="content">
+    <div class="container-fluid mt-3">
+      <!-- SELECT2 EXAMPLE -->
+      <div class="card card-default">
+        <div class="card-header">
+          <h3 class="card-title">To be Changed</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            
+          </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+        
+          <!-- /.row -->
+
+          <h5>Custom Color Variants</h5>
+          
+          <!-- /.row -->
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Visit <a href="https://select2.github.io/">Select2 documentation</a> for more examples and information about
+          the plugin.
+        </div>
+      </div>
+      <!-- /.card -->
+
+      <!-- SELECT2 EXAMPLE -->
+      
+      <!-- /.card -->
+
+      
+      <!-- /.card -->
+
+     
+      <!-- /.row -->
+      
+      <!-- /.row -->
+      
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
 @endsection
