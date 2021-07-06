26744629ec6074c2358c97bc59c89adc21a23791
Mickey Hailu <mickeyhailu20@gmail.com>
2021-07-06 12:20:04 +0300

    test

--- resources/views/admin/curriculum/view_stream.blade.php
+++ resources/views/admin/curriculum/view_stream.blade.php
@@ -62,7 +62,7 @@
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">View Class Labels</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">View Class Labels  git is a monster</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
