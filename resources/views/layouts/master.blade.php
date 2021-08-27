f5217cf6767e7c67088665815a71d1d4eff7d7da
WIN10X64 <abrishzenebe7@gmail.com>
2021-08-27 10:16:13 +0300

    xc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -177,24 +177,20 @@
                 </a>
             </div>
 
-
-
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
 
-
                 <div class="user-panel mt-1 mb-1 d-flex">
                     <span class="info text-black-50"> Navigation </span>
                 </div>
-
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+                        with font-awesome or any other icon font library -->
 
                         <li class="nav-item">
                             <a href="{{ url('dashboard') }}" class="nav-link active">
